0d602ebbb83623c463ab49b968a0237626772395
Klementino75 <klementino75@ya.ru>
2024-12-05 21:59:27 +0300

    Fixed bugs.

--- src/test/java/ru/netology/springbootauthorization/SpringBootAuthorizationApplicationTests.java
+++ src/test/java/ru/netology/springbootauthorization/SpringBootAuthorizationApplicationTests.java
@@ -3,10 +3,12 @@ package ru.netology.springbootauthorization;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -22,14 +24,15 @@ class SpringBootAuthorizationApplicationTests {
             .withExposedPorts(8080);
 
     @Test
-    @DisplayName("Test entry.")
+    @DisplayName("Login test.")
     void contextLoads() {
         final String expected = "[\"READ\",\"WRITE\",\"DELETE\"]";
         final String authorizeUser = "/authorize?user=Andrew&password=1234";
         Integer appPort = genContainer.getMappedPort(8080);
         ResponseEntity<String> result = restTemplate.getForEntity("http://localhost:" + appPort + authorizeUser, String.class);
-        System.out.println("expected = " + expected);
-        System.out.println("result = " + result.getBody());
+//        System.out.println("expected = " + expected);
+//        System.out.println("result = " + result.getBody());
         Assertions.assertEquals(expected, result.getBody());
+        System.out.println("Test Ok!\n->" + expected + " = " + result);
     }
 }
